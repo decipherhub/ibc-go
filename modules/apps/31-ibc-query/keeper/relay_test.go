4942f0088713ce24af3397c84e5b86025b76a11e
Woojinger <tlsdnwls0511@naver.com>
2022-09-24 02:05:59 +0900

    feat: implement onRecPacket func and test

--- modules/apps/31-ibc-query/keeper/relay_test.go
+++ modules/apps/31-ibc-query/keeper/relay_test.go
@@ -2,52 +2,38 @@ package keeper_test
 
 import (
 	"fmt"
-
 	"github.com/cosmos/ibc-go/v4/modules/apps/31-ibc-query/types"
 	clienttypes "github.com/cosmos/ibc-go/v4/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
-	host "github.com/cosmos/ibc-go/v4/modules/core/24-host"
 	ibctesting "github.com/cosmos/ibc-go/v4/testing"
+	"time"
 )
 
-const (
-	queryID = "query-1"
-)
-
-// test querying from chainA to chainB using both coin that orignate on
-// chainA and coin that orignate on chainB
+// test querying from chainA to chainB
 func (suite *KeeperTestSuite) TestSendQuery() {
 	var (
 		query types.CrossChainQuery
-		path   *ibctesting.Path
-		err    error
+		path  *ibctesting.Path
+		err   error
 	)
 
 	testCases := []struct {
-		msg            string
-		malleate       func()
-		sendFromSource bool
-		expPass        bool
+		msg      string
+		malleate func()
+		expPass  bool
 	}{
 		{
-			"successful query from source chain",
+			"successful query from querying chain",
 			func() {
-				suite.coordinator.CreateQueryChannels(path)
 				query = types.CrossChainQuery{
-					Id:                    queryID,
+					Id:                    "1",
 					Path:                  "test/query-1",
-					LocalTimeoutHeight:    clienttypes.NewHeight(0, 110),
-					LocalTimeoutTimestamp: 0,
-					QueryHeight:           uint64(11),
+					LocalTimeoutHeight:    clienttypes.NewHeight(0, uint64(suite.chainA.CurrentHeader.Height+1)),
+					LocalTimeoutTimestamp: uint64(suite.chainA.CurrentHeader.Time.UnixNano() + 100),
+					QueryHeight:           uint64(suite.chainB.CurrentHeader.Height),
 					ClientId:              suite.chainA.SenderAccount.GetAddress().String(),
 				}
-			}, true, true,
-		},
-		{
-			"successful query from counterparty chain",
-			func() {
-				suite.coordinator.CreateQueryChannels(path)
-			}, false, true,
+			}, true,
 		},
 	}
 
@@ -57,35 +43,13 @@ func (suite *KeeperTestSuite) TestSendQuery() {
 		suite.Run(fmt.Sprintf("Case %s", tc.msg), func() {
 			suite.SetupTest() // reset
 			path = NewQueryPath(suite.chainA, suite.chainB)
-			suite.coordinator.SetupConnections(path)
+			suite.coordinator.Setup(path)
+			//suite.coordinator.SetupConnections(path)
+			//suite.coordinator.CreateQueryChannels(path)
 
 			tc.malleate()
 
-			if !tc.sendFromSource {
-				// send query from chainB to chainA
-				transferMsg := types.NewMsgSubmitCrossChainQuery(queryID, "path/query-1", clienttypes.NewHeight(0, 110), 0, uint64(11), suite.chainB.SenderAccount.GetAddress().String(), path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID)
-				_, err = suite.chainB.SendMsgs(transferMsg)
-				suite.Require().NoError(err) // message committed
-
-				// receive query on chainA from chainB
-				queryPacket := types.NewIBCQueryPacketData(queryID, "path/query-1", uint64(11))
-				packet := channeltypes.NewPacket(queryPacket.GetBytes(), 1, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, clienttypes.NewHeight(0, 110), 0)
-
-				// get proof of packet commitment from chainB
-				err = path.EndpointA.UpdateClient()
-				suite.Require().NoError(err)
-				packetKey := host.PacketCommitmentKey(packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-				proof, proofHeight := path.EndpointB.QueryProof(packetKey)
-
-				recvMsg := channeltypes.NewMsgRecvPacket(packet, proof, proofHeight, suite.chainA.SenderAccount.GetAddress().String())
-				_, err = suite.chainA.SendMsgs(recvMsg)
-				suite.Require().NoError(err) // message committed
-			}
-
-			err = suite.chainA.GetSimApp().IBCQueryKeeper.SendQuery(
-				suite.chainA.GetContext(), path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID,
-				query,  clienttypes.NewHeight(0, 110), 0,
-			)
+			err = suite.chainA.GetSimApp().IBCQueryKeeper.SendQuery(suite.chainA.GetContext(), path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, query)
 
 			if tc.expPass {
 				suite.Require().NoError(err)
@@ -96,28 +60,69 @@ func (suite *KeeperTestSuite) TestSendQuery() {
 	}
 }
 
-
 func (suite *KeeperTestSuite) TestOnRecvPacket() {
 
 	var (
 		successResult   = types.QueryResult_QUERY_RESULT_SUCCESS
 		failResult      = types.QueryResult_QUERY_RESULT_FAILURE
+		timeoutResult   = types.QueryResult_QUERY_RESULT_TIMEOUT
+		crossChainQuery types.CrossChainQuery
 		queryResultData []byte
+		chainAHeight    int64
+		chainATimestamp uint64
 	)
 
 	testCases := []struct {
-		msg          string
-		queryResult  types.QueryResult
-		malleate     func()
-		success      bool // success of query
-		expPass      bool
+		msg         string
+		queryResult types.QueryResult
+		malleate    func()
+		expPass     bool
+		expResult   types.QueryResult
 	}{
-		{"success receive on source chain", successResult, func() {
+		{"success receive from queried chain", successResult, func() {
+			crossChainQuery = types.CrossChainQuery{
+				Id:                    "1",
+				Path:                  "test/query-1",
+				LocalTimeoutHeight:    clienttypes.NewHeight(0, uint64(chainAHeight+50)),
+				LocalTimeoutTimestamp: chainATimestamp + uint64(time.Minute.Nanoseconds()),
+				QueryHeight:           uint64(suite.chainB.CurrentHeader.Height),
+				ClientId:              suite.chainB.CurrentHeader.ChainID,
+			}
 			queryResultData = []byte("query result data")
-		}, true, true},
-		{"fail receive on source chain", failResult, func() {
-			queryResultData = nil
-		}, false, true},
+		}, true, successResult},
+		{"fail receive from queried chain", failResult, func() {
+			crossChainQuery = types.CrossChainQuery{
+				Id:                    "2",
+				Path:                  "test/query-2",
+				LocalTimeoutHeight:    clienttypes.NewHeight(0, uint64(chainAHeight+50)),
+				LocalTimeoutTimestamp: chainATimestamp + uint64(time.Minute.Nanoseconds()),
+				QueryHeight:           uint64(suite.chainB.CurrentHeader.Height),
+				ClientId:              suite.chainB.CurrentHeader.ChainID,
+			}
+			queryResultData = []byte("query result data2")
+		}, true, failResult},
+		{"timeout because of block height", successResult, func() {
+			crossChainQuery = types.CrossChainQuery{
+				Id:                    "3",
+				Path:                  "test/query-3",
+				LocalTimeoutHeight:    clienttypes.NewHeight(0, uint64(chainAHeight+1)),
+				LocalTimeoutTimestamp: chainATimestamp + uint64(time.Minute.Nanoseconds()),
+				QueryHeight:           uint64(suite.chainB.CurrentHeader.Height),
+				ClientId:              suite.chainB.CurrentHeader.ChainID,
+			}
+			queryResultData = []byte("query result data3")
+		}, true, timeoutResult},
+		{"timeout because of block timestamp", failResult, func() {
+			crossChainQuery = types.CrossChainQuery{
+				Id:                    "4",
+				Path:                  "test/query-4",
+				LocalTimeoutHeight:    clienttypes.NewHeight(0, uint64(chainAHeight+50)),
+				LocalTimeoutTimestamp: chainATimestamp + 30,
+				QueryHeight:           uint64(suite.chainB.CurrentHeader.Height),
+				ClientId:              suite.chainB.CurrentHeader.ChainID,
+			}
+			queryResultData = []byte("query result data4")
+		}, true, timeoutResult},
 	}
 
 	for _, tc := range testCases {
@@ -129,47 +134,42 @@ func (suite *KeeperTestSuite) TestOnRecvPacket() {
 			path := NewQueryPath(suite.chainA, suite.chainB)
 			suite.coordinator.Setup(path)
 
-			seq := uint64(1)
-
-			// send query from chainA to chainB
-			queryMsg := types.NewMsgSubmitCrossChainQuery(queryID, "test/query-1", clienttypes.NewHeight(0, 110), 0, uint64(11), suite.chainA.SenderAccount.GetAddress().String(), path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID)
-			_, err := suite.chainA.SendMsgs(queryMsg)
-			query := types.CrossChainQuery{
-				Id:                    queryMsg.Id,
-				Path:                  queryMsg.Path,
-				LocalTimeoutHeight:    queryMsg.LocalTimeoutHeight,
-				LocalTimeoutTimestamp: queryMsg.LocalTimeoutStamp,
-				QueryHeight:           queryMsg.QueryHeight,
-				ClientId:              queryMsg.Sender,
-			}
-			suite.chainA.GetSimApp().IBCQueryKeeper.SetCrossChainQuery(suite.chainA.GetContext(), query)
-			suite.Require().NoError(err) // message committed
+			chainAHeight = suite.chainA.GetContext().BlockHeight()
+			chainATimestamp = uint64(suite.chainA.GetContext().BlockTime().UnixNano())
 
 			tc.malleate()
-			
+			query := crossChainQuery
+			// send query from chainA to chainB
+			queryMsg := types.NewMsgSubmitCrossChainQuery(query.Id, query.Path, query.LocalTimeoutHeight, query.LocalTimeoutTimestamp, query.QueryHeight, suite.chainA.SenderAccount.GetAddress().String(), path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID)
+			_, err := suite.chainA.SendMsgs(queryMsg)
+			suite.Require().NoError(err) // message committed
+			suite.chainA.GetSimApp().IBCQueryKeeper.SetCrossChainQuery(suite.chainA.GetContext(), query)
+
 			// query result of chain B
-			data := (types.IBCQueryResultPacketData{
-				Id:           queryID,
-				Path:         "test/query-1",
-				QueryHeight:  uint64(11),
-				Result:       tc.queryResult,
-				Data:         queryResultData,
-			})
-			
-			// receive query response packet from chain B 
+			data := types.IBCQueryResultPacketData{
+				Id:          query.Id,
+				Path:        query.Path,
+				QueryHeight: query.QueryHeight,
+				Result:      tc.queryResult,
+				Data:        queryResultData,
+			}
+
+			seq := uint64(1)
+			// receive query response packet from chain B
 			packet := channeltypes.NewPacket(data.GetBytes(), seq, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, clienttypes.NewHeight(0, 100), 0)
 			err = suite.chainA.GetSimApp().IBCQueryKeeper.OnRecvPacket(suite.chainA.GetContext(), packet)
-
+			suite.Require().NoError(err)
 			if tc.expPass {
-				suite.Require().NoError(err)
-				result, _ := suite.chainA.GetSimApp().IBCQueryKeeper.GetCrossChainQueryResult(suite.chainA.GetContext(), queryID)
-
-				if tc.success {
-					suite.Require().Equal("query result data", string(result.GetData()))
-					suite.Require().Equal("QUERY_RESULT_SUCCESS", result.GetResult().String())
-				} else {
-					suite.Require().Equal("", string(result.GetData()))
-					suite.Require().Equal("QUERY_RESULT_FAILURE", result.GetResult().String())
+				result, isUnmarshal := suite.chainA.GetSimApp().IBCQueryKeeper.GetCrossChainQueryResult(suite.chainA.GetContext(), query.Id)
+				suite.Require().True(isUnmarshal)
+				suite.Require().Equal(queryResultData, result.GetData())
+				switch tc.expResult {
+				case successResult:
+					suite.Require().Equal(successResult, result.GetResult())
+				case failResult:
+					suite.Require().Equal(failResult, result.GetResult())
+				case timeoutResult:
+					suite.Require().Equal(timeoutResult, result.GetResult())
 				}
 			} else {
 				suite.Require().Error(err)
@@ -177,7 +177,6 @@ func (suite *KeeperTestSuite) TestOnRecvPacket() {
 		})
 	}
 }
-
 
 func (suite *KeeperTestSuite) TestOnAcknowledgementPacket() {
 	var (
@@ -193,8 +192,8 @@ func (suite *KeeperTestSuite) TestOnAcknowledgementPacket() {
 		success  bool // success of ack
 		expPass  bool
 	}{
-		{"success ack causes no-op", successAck, func() {	}, true, true},
-		{"successful emit error", failedAck, func() {   }, false, false},
+		{"success ack causes no-op", successAck, func() {}, true, true},
+		{"successful emit error", failedAck, func() {}, false, false},
 	}
 
 	for _, tc := range testCases {
@@ -217,11 +216,9 @@ func (suite *KeeperTestSuite) TestOnAcknowledgementPacket() {
 	}
 }
 
-
-
 func (suite *KeeperTestSuite) TestOnTimeoutPacket() {
 	var (
-		path   *ibctesting.Path
+		path *ibctesting.Path
 	)
 
 	testCases := []struct {
@@ -230,7 +227,7 @@ func (suite *KeeperTestSuite) TestOnTimeoutPacket() {
 		expPass  bool
 	}{
 		{
-			"successful timeout from sender as source chain", func() { }, true,
+			"successful timeout from sender as source chain", func() {}, true,
 		},
 	}
 
--- modules/apps/31-ibc-query/types/errors.go
+++ modules/apps/31-ibc-query/types/errors.go
@@ -7,9 +7,9 @@ import (
 // 31-ibc-query sentinel errors
 var (
 	ErrInvalidVersion          = sdkerrors.Register(ModuleName, 2, "invalid 31-IBC-query version")
-	ErrInvalidTimeoutHeight    = sdkerrors.Register(ModuleName, 3, "invalid timeout height")
-	ErrCrossChainQueryNotFound = sdkerrors.Register(ModuleName, 4, "no query found for given query id")
-	ErrQuerytTimeout           = sdkerrors.Register(ModuleName, 5, "query timeout")
+	ErrInvalidQuery            = sdkerrors.Register(ModuleName, 3, "invalid cross chain query")
+	ErrTimeout                 = sdkerrors.Register(ModuleName, 4, "cross chain query timeout")
+	ErrCrossChainQueryNotFound = sdkerrors.Register(ModuleName, 5, "no query found for given query id")
 	ErrMaxTransferChannels     = sdkerrors.Register(ModuleName, 6, "max transfer channels")
 	ErrUnknownDataType         = sdkerrors.Register(ModuleName, 7, "unknown data type")
 )
