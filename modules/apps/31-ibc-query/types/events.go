c49ca0cd9f04204f74a5f8ed5b63bcc471cf624a
sujine2 <twinsujin22@gmail.com>
2022-09-25 19:26:05 +0900

    refactor: Remove event proto

--- modules/apps/31-ibc-query/types/events.go
+++ modules/apps/31-ibc-query/types/events.go
@@ -3,7 +3,6 @@ package types
 import (
 	fmt "fmt"
 
-	clienttypes "github.com/cosmos/ibc-go/v4/modules/core/02-client/types"
 	host "github.com/cosmos/ibc-go/v4/modules/core/24-host"
 )
 
@@ -23,15 +22,3 @@ var (
 	AttributeValueCategory = fmt.Sprintf("%s_%s", host.ModuleName, ModuleName)
 )
 
-func NewEventQuerySubmitted(
-	id, path string,
-	localTimeoutHeight clienttypes.Height,
-	localTimeoutStamp, queryHeight uint64) *EventQuerySubmitted {
-	return &EventQuerySubmitted{
-		Id:                 id,
-		Path:               path,
-		LocalTimeoutHeight: localTimeoutHeight,
-		LocalTimeoutStamp:  localTimeoutStamp,
-		QueryHeight:        queryHeight,
-	}
-}
